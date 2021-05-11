f20b08224e8d87982ce6da5b7c208258cae7e66d
manuelzea <manuel.zea@pureweb.com>
2021-05-11 14:33:08 -0600

    trying something new

---
+++ .generated.config.js
@@ -0,0 +1,168 @@
+module.exports = {
+    "metadata": {
+        "name": "PureWeb",
+        "short_name": "PW",
+        "description": "PureWeb",
+        "language": "en",
+        "url": "http://localhost",
+        "pathPrefix": "/",
+        "gaTrackingId": null,
+        "siteImage": null,
+        "favicon": "/assets/favicon-32x32.png",
+        "themeColor": "#0066cc"
+    },
+    "header": {
+        "logo": "/assets/logoPureweb.png",
+        "logoLink": "/",
+        "helpUrl": "",
+        "links": [],
+        "enabled": true
+    },
+    "sidebar": {
+        "enabled": true,
+        "ignoreIndex": false,
+        "forcedNavOrder": [],
+        "expanded": [],
+        "groups": [
+            {
+                "order": 1,
+                "path": "/gettingstarted",
+                "title": "Getting Started"
+            },
+            {
+                "order": 2,
+                "path": "/configuration",
+                "title": "Configuration"
+            }
+        ],
+        "links": [],
+        "poweredBy": {}
+    },
+    "pwa": {
+        "manifest": {
+            "name": "BooGi",
+            "short_name": "BooGi",
+            "start_url": "/",
+            "background_color": "#6b37bf",
+            "theme_color": "#6b37bf",
+            "display": "minimal-ui",
+            "crossOrigin": "anonymous",
+            "icon": "static/assets/logoPureweb.png",
+            "description": "PureWeb",
+            "cache_busting_mode": "none",
+            "include_favicon": false,
+            "lang": "en"
+        },
+        "enabled": true
+    },
+    "social": {
+        "facebook": "",
+        "github": "",
+        "gitlab": "",
+        "instagram": "",
+        "linkedin": "",
+        "mail": "",
+        "gmail": "",
+        "slack": "",
+        "twich": "",
+        "twitter": "",
+        "youtube": ""
+    },
+    "features": {
+        "editOnRepo": {
+            "editable": true,
+            "location": "http://localhost",
+            "type": "github"
+        },
+        "search": {
+            "enabled": true,
+            "indexName": "docs",
+            "algoliaAppId": null,
+            "algoliaSearchKey": null,
+            "algoliaAdminKey": null,
+            "excerptSize": 20000,
+            "engine": "localsearch",
+            "placeholder": "Search",
+            "startComponent": "icon",
+            "debounceTime": 380,
+            "snippetLength": 23,
+            "hitsPerPage": 10,
+            "showStats": true,
+            "localSearchEngine": {
+                "encode": "advanced",
+                "tokenize": "full",
+                "threshold": 2,
+                "resolution": 30,
+                "depth": 20
+            },
+            "pagination": {
+                "enabled": true,
+                "totalPages": 10,
+                "showNext": true,
+                "showPrevious": true
+            }
+        },
+        "toc": {
+            "show": true,
+            "depth": 3
+        },
+        "previousNext": {
+            "enabled": true,
+            "arrowKeyNavigation": true
+        },
+        "scrollTop": true,
+        "showMetadata": true,
+        "propagateNetlifyEnv": true,
+        "pageProgress": {
+            "enabled": false,
+            "excludePaths": [
+                "/"
+            ],
+            "height": 3,
+            "prependToBody": false,
+            "color": "#A05EB5"
+        },
+        "mermaid": {
+            "language": "mermaid",
+            "theme": "dark",
+            "options": {},
+            "width": 300,
+            "height": 300
+        },
+        "rss": {
+            "enabled": false,
+            "showIcon": true,
+            "title": "My RSS feed",
+            "copyright": "",
+            "webMaster": "M",
+            "managingEditor": "",
+            "categories": [
+                "GatsbyJS",
+                "Docs"
+            ],
+            "ttl": "60",
+            "matchRegex": "^/",
+            "outputPath": "/rss.xml",
+            "generator": "gidocs"
+        },
+        "darkMode": {
+            "enabled": true,
+            "default": false
+        },
+        "publishDraft": false,
+        "fullScreenMode": {
+            "enabled": false,
+            "hideHeader": true,
+            "hideToc": true,
+            "hideSidebar": true
+        }
+    },
+    "search": {
+        "enabled": true,
+        "startComponent": "input",
+        "placeholder": "Search...",
+        "pagination": {
+            "enabled": true
+        }
+    }
+};
